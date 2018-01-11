5dc044f656292b51dda2fae690ee80c196c59daf
Stella <sgordonzigel@gmail.com>
2018-01-11 09:44:51 -0500

    remove canvas from ImageUploadContainer, uploads and filters working. create ImageUploadContainer.scss to hold filter info - moved from Image.scss

--- src/components/ImageUploadContainer/ImageUploadContainer.js
+++ src/components/ImageUploadContainer/ImageUploadContainer.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 
-// import '../Image/Image.css'
 import './ImageUploadContainer.css'
 
 class ImageUploadContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      imgUrl: 'https://hdwallsource.com/img/2014/5/beach-scenery-18736-19215-hd-wallpapers.jpg',
-      imageUpload: false,
-      file: '',
-      createImage: ''
+      imgUrl: 'https://hdwallsource.com/img/2014/5/beach-scenery-18736-19215-hd-wallpapers.jpg'
     }
 
     this.handleImageUpload = this.handleImageUpload.bind(this)
@@ -30,31 +26,15 @@ class ImageUploadContainer extends Component {
     }
   }
 
-
-  componentDidMount() {
-    let image = new Image()
-    image.src = this.state.imgUrl
-    let canvas = this.refs.canvas
-    canvas.width = image.width
-    canvas.height = image.height
-    let ctx = canvas.getContext('2d')
-    ctx.drawImage(image, 0, 0)
-  }
-
-
   render() {
     return(
       <div>
-        <figure id="images" alt="test">
-          <img src={this.state.imgUrl} className='starting-image'/>
-        </figure>
-
+        <img src={this.state.imgUrl} className='starting-image' id='images'/>
         <div>
           <form onSubmit={this.handleSubmit}>
-           <input type="file" onChange={this.handleImageUpload} />
+            <input type="file" onChange={this.handleImageUpload} />
           </form>
         </div>
-          <canvas ref="canvas" className="canvas" />
       </div>
     )
   }
